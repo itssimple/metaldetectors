3727ee6c23e3a26bd9c4f7277b95f43a7902fd86
NoLifeKing <github-things@nolifeking85.tv>
2024-07-20 21:14:34 +0200

    chore: Fix unused code.

--- src/main/java/se/itssimple/metaldetectors/SimpleMetalDetector.java
+++ src/main/java/se/itssimple/metaldetectors/SimpleMetalDetector.java
@@ -1,13 +1,9 @@
 package se.itssimple.metaldetectors;
 
 import net.minecraft.world.item.Item;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber
 public class SimpleMetalDetector extends Item {
     public SimpleMetalDetector(Properties properties) {
         super(properties);
     }
-
-
 }
